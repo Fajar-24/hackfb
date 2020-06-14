799bed768e4f34777f8473a45e23c08a372bea4b
https://github.com/Fajar-24 <65275087+Fajar-24@users.noreply.github.com>
2020-06-15 04:11:33 +0700

    Create hack.py

---
+++ hack.py
@@ -0,0 +1,51 @@
+
+#!/bin/bash
+clear
+#HAYO MAU NGERECODE YA NJINK
+#BNGST LO
+#ASU
+figlet -f slant "HACK FB"|lolcat
+echo "~~~~~~~~~~~~~~~~~~~~~HACK~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "AUTHOR   :  FAJAR CYBER"|lolcat
+echo "KONTAK   :  08817805411"|lolcat
+echo "GITHUB   :  FAJAR-24 "|lolcat
+echo "~~~~~~~~~~~~~~~~~~~FACEBOOK~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo
+echo "Login Facebook Anda"
+echo
+read -p "USERNAME/EMAIL  : " read tr
+read -p "PASSWORD  : " read tr
+echo "SEDANG MASUKKK..."
+echo "BERHASIL LOGIN ✓" |lolcat
+clear
+
+figlet -f slant "FAJAR CYBER"|lolcat
+echo "~~~~~~~~~~~~~~~~~~~~~HACK~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "AUTHOR   :  FAJAR CYBER"|lolcat
+echo "KONTAK   :  08817805411"|lolcat
+echo "GITHUB   :  FAJAR-24 "|lolcat
+echo "~~~~~~~~~~~~~~~~~~~FACEBOOK~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo
+echo "PILIH MENU HACK FB"|lolcat
+echo
+echo $b "_____________________________________________________"|lolcat
+echo $b "1.CRACK DARI TEMAN [TARGET]${enda}";
+echo $b "2.CRACK DARI GRUPP${enda}";
+echo $b "3.BRUTEFORCE [TARGET]${enda}";
+echo $b "4.MULTI BRUTEFORCE${enda}";
+echo $b "_____________________________________________________"|lolcat
+echo
+read -p "»" pil;
+
+case $pil in
+1)
+
+;;
+2)
+
+
+;;
+3)
+
+
+;;
